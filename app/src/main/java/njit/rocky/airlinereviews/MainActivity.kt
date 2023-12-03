c2cd56a7339ccd7fea1361d814200da9b9e04129
Rocky Haotian Du <2712479005@qq.com>
2023-12-03 12:56:18 +0800

    Update MainActivity.xml [Airline_Reviews.app.main]

--- app/src/main/java/njit/rocky/airlinereviews/MainActivity.kt
+++ app/src/main/java/njit/rocky/airlinereviews/MainActivity.kt
@@ -1,12 +1,12 @@
 package njit.rocky.airlinereviews
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import njit.rocky.airlinereviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +26,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
