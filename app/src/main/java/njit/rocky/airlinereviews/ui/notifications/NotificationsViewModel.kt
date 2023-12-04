8893585dde9b8916eaed608581cabab0263819f8
Rocky Haotian Du <2712479005@qq.com>
2023-12-04 21:58:09 +0800

    Update NotificationsViewModel.kt, NotificationsFragment.kt [Airline_Reviews.app.main]

--- app/src/main/java/njit/rocky/airlinereviews/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/njit/rocky/airlinereviews/ui/notifications/NotificationsViewModel.kt
@@ -1,13 +1,5 @@
 package njit.rocky.airlinereviews.ui.notifications
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class NotificationsViewModel : ViewModel()
