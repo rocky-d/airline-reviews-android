146b697709c4e3009e159b7b1465ffff8b174cec
Rocky Haotian Du <2712479005@qq.com>
2023-12-04 11:55:02 +0800

    Update DashboardViewModel.kt, DashboardFragment.kt [Airline_Reviews.app.main]

--- app/src/main/java/njit/rocky/airlinereviews/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/njit/rocky/airlinereviews/ui/dashboard/DashboardFragment.kt
@@ -4,39 +4,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import njit.rocky.airlinereviews.databinding.FragmentDashboardBinding
+import njit.rocky.airlinereviews.R
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var viewModel: DashboardViewModel
+    private lateinit var editText1: EditText // Initialize these with appropriate IDs
+    private lateinit var imageViewPicture: ImageView
+    private lateinit var buttonSubmit: Button
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        editText1 = view.findViewById(R.id.editText1)
+        // Initialize other views
+        imageViewPicture = view.findViewById(R.id.imageViewPicture)
+        buttonSubmit = view.findViewById(R.id.buttonSubmit)
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        buttonSubmit.setOnClickListener {  // TODO: have some problem now
+            val airlineName = editText1.text.toString()
+            if (airlineName.isEmpty()) {
+                Toast.makeText(requireActivity(), "Empty Airline Name", Toast.LENGTH_SHORT).show()
+            }
+            viewModel.fetchDataFromServer(airlineName)
         }
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        viewModel.imageData.observe(viewLifecycleOwner) { bitmap ->
+            imageViewPicture.setImageBitmap(bitmap)
+        }
+
+        return view
     }
 }
--- app/src/main/java/njit/rocky/airlinereviews/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/njit/rocky/airlinereviews/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,56 @@
 package njit.rocky.airlinereviews.ui.dashboard
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.ByteArrayOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class DashboardViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val _imageData = MutableLiveData<Bitmap>()
+    val imageData: LiveData<Bitmap> = _imageData
+
+    fun fetchDataFromServer(airlineName: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val response = sendDataToServer(createJsonData(airlineName))
+
+            val bitmap = BitmapFactory.decodeByteArray(response, 0, response.size)
+            _imageData.postValue(bitmap)
+        }
     }
-    val text: LiveData<String> = _text
+
+    private fun createJsonData(airlineName: String): String {
+        return "hello"
+    }
+
+    private suspend fun sendDataToServer(jsonData: String): ByteArray {
+        val url = URL("http://10.0.2.2:5000/android")
+        val connection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "POST"
+        connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8")
+        connection.doOutput = true
+
+        val outputStream = BufferedOutputStream(connection.outputStream)
+        outputStream.write(jsonData.toByteArray())
+        outputStream.flush()
+
+        val inputStream = BufferedInputStream(connection.inputStream)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+
+        inputStream.use {
+            it.copyTo(byteArrayOutputStream)
+        }
+
+        return byteArrayOutputStream.toByteArray()
+    }
 }
