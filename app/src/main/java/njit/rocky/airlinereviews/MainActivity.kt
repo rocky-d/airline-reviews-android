0c771f83588eb003f4d0a50c9be212277169b1e1
Rocky Haotian Du <2712479005@qq.com>
2023-12-18 16:52:37 +0800

    Update MainActivity.kt [Airline_Reviews.app.main]

--- app/src/main/java/njit/rocky/airlinereviews/MainActivity.kt
+++ app/src/main/java/njit/rocky/airlinereviews/MainActivity.kt
@@ -1,7 +1,6 @@
 package njit.rocky.airlinereviews
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
